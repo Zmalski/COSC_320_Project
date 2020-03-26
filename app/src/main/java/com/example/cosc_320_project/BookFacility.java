87564d6fafd29e009b95fcc6f3c8a1618be84d41
kober-bober <koby.anderson@live.com>
2020-03-26 14:04:53 -0700

    updated trainer selection page

--- app/src/main/java/com/example/cosc_320_project/BookFacility.java
+++ app/src/main/java/com/example/cosc_320_project/BookFacility.java
@@ -1,17 +1,17 @@
 package com.example.cosc_320_project;
 
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBarDrawerToggle;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.drawerlayout.widget.DrawerLayout;
-
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.google.android.material.navigation.NavigationView;
 
