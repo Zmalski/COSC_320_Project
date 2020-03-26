dc430bf861ffc72dba62ee6e61ecca0a4c935899
kober-bober <koby.anderson@live.com>
2020-03-26 11:11:44 -0700

    added trainer images

--- app/src/main/java/com/example/cosc_320_project/TrainerListActivity.java
+++ app/src/main/java/com/example/cosc_320_project/TrainerListActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class TrainerActivityTest extends AppCompatActivity {
+public class TrainerListActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_trainer_test);
+        setContentView(R.layout.activity_trainer_list);
     }
 }
