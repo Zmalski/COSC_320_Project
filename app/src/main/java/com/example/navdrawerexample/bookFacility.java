d7d1ec52506e7bb99a000493dc96cb61c8cb6c04
Zach <zacharymaludzinski@gmail.com>
2020-04-02 05:37:01 -0700

    booking functionality nearly complete

--- app/src/main/java/com/example/navdrawerexample/bookFacility.java
+++ app/src/main/java/com/example/navdrawerexample/bookFacility.java
@@ -3,27 +3,71 @@ package com.example.navdrawerexample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Switch;
 import android.widget.TextView;
 
 public class bookFacility extends AppCompatActivity {
 
     TextView day;
     TextView time;
-    TextView place;
-    TextView activty;
+    TextView facility;
+    TextView area;
+    LinearLayout scrollArea;
+    Boolean scrollPopulated;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_facility);
 
+        // Initialize views
         day = findViewById(R.id.day);
         time = findViewById(R.id.time);
-        place = findViewById(R.id.place);
-        activty = findViewById(R.id.activity);
+        facility = findViewById(R.id.place);
+        area = findViewById(R.id.activity);
+        scrollArea = findViewById(R.id.scrollLayout);
+        scrollPopulated = false;
+
+        // Set time to a more readable format
+        String t = (String) getIntent().getExtras().get("time");
+        String[] splitTime = t.split("-");
+        t = splitTime[0] + ":00 - " + splitTime[1] + ":00";
+
+        // Capitalize first letter of the area
+        String a = (String) getIntent().getExtras().get("activity");
+        a = a.substring(0, 1).toUpperCase() + a.substring(1);
+
+        // Update Views
         day.setText((String) getIntent().getExtras().get("day"));
-        time.setText((String) getIntent().getExtras().get("time"));
-        place.setText((String) getIntent().getExtras().get("place"));
-        activty.setText((String) getIntent().getExtras().get("activity"));
+        time.setText(t);
+        facility.setText((String) getIntent().getExtras().get("place"));
+        area.setText(a);
+
+        final Switch equipSwitch = findViewById(R.id.equipSwitch);
+
+        equipSwitch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (equipSwitch.isChecked()) {
+                    //TODO: Display hidden Equipment rental screen
+                    if (scrollPopulated == false) {
+                        for (int i = 0; i < 50; i++) {
+                            TextView textView = new TextView(bookFacility.this);
+                            textView.setText("Test" + i);
+                            scrollArea.addView(textView);
+                        }
+                        scrollPopulated = true;
+                    }
+                }
+
+            }
+        });
+
+    }
+
+    public void cancel(View view) {
+        finish();
     }
 }
